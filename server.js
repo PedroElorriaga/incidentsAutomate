d52801f22d601983f08650b0708e1afcc1763d75
PedroElorriaga <pedrohes2002@outlook.com>
2023-10-26 19:18:17 -0300

    Teste em PROD

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./src/app/app');
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is now running on http://localhost:3000`)
+    console.log(`Server is now running`)
 })
